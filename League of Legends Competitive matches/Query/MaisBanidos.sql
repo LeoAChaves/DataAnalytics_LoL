65442361e34dee0b93db3b03fae212b7fe247597
vanessacreis <vanessacreis@live.com>
2022-02-05 17:05:02 -0300

    Query 10 campeões mais banidos

---
+++ League of Legends Competitive matches/Query/MaisBanidos.sql
@@ -0,0 +1,16 @@
+-- Os 10 campeões mais banidos
+
+SELECT  
+    ban as mais_manidos,
+     COUNT(ban) AS countBan
+FROM
+    (SELECT ban_1 AS ban FROM bans
+     UNION ALL
+     SELECT ban_2 AS ban FROM bans
+     UNION ALL
+     SELECT ban_3 AS ban FROM bans
+    ) as totalBans
+GROUP BY ban
+HAVING COUNT(ban) > 0 AND ban != '' 
+order by countBan desc
+limit 10;
