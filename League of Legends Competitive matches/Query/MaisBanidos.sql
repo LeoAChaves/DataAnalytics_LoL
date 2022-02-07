d0f454f2a77eb98eccd509e0aa9ab0091f605a11
vanessacreis <vanessacreis@live.com>
2022-02-07 15:59:38 -0300

    Inner join com a tabela de campeões

--- League of Legends Competitive matches/Query/MaisBanidos.sql
+++ League of Legends Competitive matches/Query/MaisBanidos.sql
@@ -1,7 +1,9 @@
 -- Os 10 campeões mais banidos
 
 SELECT  
-    ban as mais_manidos,
+    ban as mais_banidos,
+    champ.classe_primaria,
+    champ.Classe_Secundaria,
      COUNT(ban) AS countBan
 FROM
     (SELECT ban_1 AS ban FROM bans
@@ -10,6 +12,8 @@ FROM
      UNION ALL
      SELECT ban_3 AS ban FROM bans
     ) as totalBans
+inner join champions champ
+on ban = champ.nome
 GROUP BY ban
 HAVING COUNT(ban) > 0 AND ban != '' 
 order by countBan desc
